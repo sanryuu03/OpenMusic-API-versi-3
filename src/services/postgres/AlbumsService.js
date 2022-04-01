ec1661384334d56eeccf522314b8f57dac61f640
sanryuu03 <ryuuosku@gmail.com>
2022-04-01 22:27:28 +0700

    OpenMusic API versi 1

---
+++ src/services/postgres/AlbumsService.js
@@ -0,0 +1,73 @@
+const {Pool} = require('pg');
+const {nanoid} = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const {mapDBToModel} = require('../../utils');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class AlbumsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbums({name, year}) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id',
+      values: [id, name, year, createdAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Albums gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getAlbumsById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Albums tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModel)[0];
+  }
+
+  async editAlbumsById(id, {name, year}) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE albums SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id',
+      values: [name, year, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui albums. Id tidak ditemukan');
+    }
+  }
+
+  async deleteAlbumsById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Albums gagal dihapus. Id tidak ditemukan');
+    }
+  }
+}
+
+module.exports = AlbumsService;
